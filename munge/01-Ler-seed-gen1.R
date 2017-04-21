0436a8f3bdfdbdea21a09baa19602ffc1a3271f7
Bruce Kendall <kendall@bren.ucsb.edu>
2017-04-21 09:47:53 -0700

    Fixes to Ler gen1 calculations

--- munge/01-Ler-seed-gen1.R
+++ munge/01-Ler-seed-gen1.R
@@ -8,9 +8,16 @@ Ler1BC$Density[Ler1BC$Treatment == "C"] <- 50
 # We calculate the "effective seed number" as the number falling in the mother pot plus
 # twice the number falling in the other pots (the latter gets at bidirectional dispersal,
 # and estimates the number of seeds that would fall somewhere on the runway if the home
-# pot was in the middle of the runway):
-Ler_eff_seed_gen1 <- group_by(Ler1BC, ID, Density) %>% 
-  summarise(eff_sd_no = sum(Seedlings) + sum(Seedlings * Pot > 0))
+# pot was in the middle of the runway)
+#
+# We also record the number staying in the home pot and the number dispersing,
+#   and the fraction staying home
+Ler_seed_gen1 <- group_by(Ler1BC, ID, Density) %>% 
+  summarise(eff_sd_no = sum(Seedlings) + 2 * sum(Seedlings[Pot > 0]),
+            home = Seedlings[Pot == 0],
+            away = sum(Seedlings[Pot > 0]),
+            stay_prop = home / (home + away)
+            )
 
 # Clean up
 rm("Ler1BC")
