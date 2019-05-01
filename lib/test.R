bd76d6c02b5ebcc81d363eb5b9542f8ec38d505f
Bruce Kendall <kendall@bren.ucsb.edu>
2019-05-01 15:58:53 -0700

    Tests for constant kernel (pass)

--- lib/test.R
+++ lib/test.R
@@ -14,8 +14,9 @@ test_det_kernel <- function() {
                  frac_dispersing = 0.75)
   controls <- list(pot_width = 7)
   
+  ### Tests for constant kernel
   with(params, plot(dgengamma(0:30, gg_mu, gg_sigma, gg_Q)))
-  
+  with(params, print(pgengamma(14, gg_mu, gg_sigma, gg_Q, lower = FALSE)  * 75/2))
   array_dim <- c(nrep, npot)
   kernel_params <- list(
     frac_dispersing = array(params$frac_dispersing, array_dim),
@@ -24,6 +25,16 @@ test_det_kernel <- function() {
     gg_Q            = array(params$gg_Q,            array_dim)
   )
   
-  print(det_kernel(Seeds, kernel_params, params, controls))
-    
+  d1 <- det_kernel(Seeds, kernel_params, params, controls)
+  # should be identical for all pots/reps
+  # home_pot should be 25
+  # forward and backward should be identical
+  # max_dist should be 6
+  print(d1)
+  
+  # Expected number of seeds beyond pot 2:
+  with(params, print(pgengamma(14, gg_mu, gg_sigma, gg_Q, lower = FALSE)  * 75/2))
+  
+  # Expected number of seeds placed in pots 3-6 (should be very close to previous)
+  sum(d1$forward_dispersal[1,1,3:6])
 }
