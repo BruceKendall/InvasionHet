96aff620dce7e289cde3f3eeb91db4b183c3b2f7
Bruce Kendall <kendall@bren.ucsb.edu>
2019-05-12 16:15:44 -0700

    Save testing commands

---
+++ src/scratch.R
@@ -0,0 +1,3 @@
+# Try out the various combinations of control variables with ler-like parameters
+
+test_iterate_genotype(4, ES_seeds = FALSE,DS_seeds = FALSE, kernel_stoch = FALSE, seed_sampling = FALSE,kernel_stoch_pots = FALSE, gap_size = 0)
